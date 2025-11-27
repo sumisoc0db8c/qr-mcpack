eae237d545eac4b8094a1620a4d30f720673456f
sumiso_c0db8c <220422087+sumisoc0db8c@users.noreply.github.com>
2025-11-27 22:19:40 +0900

    デザイン調整

--- src/App.tsx
+++ src/App.tsx
@@ -35,32 +35,45 @@ function App() {
 
   };
 
-  return (
-    <div className="flex flex-col">
-      <input
-        className="border p-2 mb-4"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="QRコードにしたい文字列"
+return (
+  <div className="flex flex-col mx-auto p-6 space-y-4 w-full">
+    <input
+      className="border rounded-md p-2 focus:ring-2 focus:ring-blue-400 bg-white"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="QRコードにしたい文字列"
+    />
+    
+    <button
+      className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+      onClick={handleGenerate}
+    >
+      生成
+    </button>
+
+    {image ? (
+      <img
+        src={image}
+        alt="QR Code"
+        className="mt-4 mx-auto border rounded-md shadow-md w-48 h-48"
       />
-      <button className="p-2 bg-blue-500 text-white mb-4" onClick={handleGenerate}>
-        生成
-      </button>
+    ):(
+      <div className="mt-4 w-48 h-48" />
+    )}
 
-      {image && <img src={image} alt="QR Code" className="mt-4" />}
-
-      {/* ダウンロードボタン */}
-      {downloadUrl && (
-        <a
-          href={downloadUrl}
-          download="QRコード生成アドオン.mcpack"
-          className="p-2 bg-green-500 text-white text-center"
-        >
-          ダウンロード
-        </a>
-      )}
-    </div>
-  );
+    {downloadUrl ? (
+      <a
+        href={downloadUrl}
+        download="QRコード生成アドオン.mcpack"
+        className="p-2 h-10 bg-green-500 text-white text-center rounded-md hover:bg-green-600"
+      >
+        ダウンロード
+      </a>
+    ):(
+      <div className="p-2 h-10" />
+    )}
+  </div>
+);
 }
 
 export default App;
